3c0383ac0d9db18f3efdc9cbfd94834a17d055a6
gsgamingteam <gsgamingteam@gmail.com>
2020-12-03 12:01:03 +0200

    moved the directory data into its own reducer, created fully functioning Checkout page

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import React from 'react';
 import './App.css';
 
-import {Route, Switch} from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selector'
 
 class App extends React.Component {
-constructor(){
-  super();
-
-  this.state = {
-    currentUser: null
-  }
-}
-
 unsubscribeFromAuth = null
 
 componentDidMount (){
+const { setCurrentUser} = this.props;
+
  this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
   if(userAuth){
     const userRef = await   createUserProfileDocument(userAuth);
 
     userRef.onSnapshot(snapShot => {
-      this.setState({
-        currentUser: {
+      setCurrentUser({
           id: snapShot.id,
           ...snapShot.data()
-        }
-      });
+        });
     });
   }
 
-    this.setState({currentUser: userAuth});
+  setCurrentUser(userAuth);
   
   });
 }
@@ -47,11 +44,12 @@ componentWillUnmount(){
   render() {
     return (
       <div>
-        <Header currentUser = {this.state.currentUser} />
+        <Header />
         <Switch>
         <Route exact path = '/' component = {HomePage} />
         <Route path = '/shop' component = {ShopPage} />
-        <Route path = '/signIn' component = {SignInAndSignUp} />
+        <Route  path = '/checkout' component = {CheckoutPage} />
+        <Route exact path = '/signIn' render={() => this.props.currentUser ? (<Redirect to ='/' /> ) : (<SignInAndSignUp />)} />
         </Switch>
   
       </div>
@@ -59,4 +57,11 @@ componentWillUnmount(){
   }
 }
 
-export default App;
+const mapStateToProps = createStructuredSelector ({
+  currentUser: selectCurrentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+export default connect(mapStateToProps, mapDispatchToProps, null )(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
+
 import './index.css';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+
+ReactDOM.render(  
+  <Provider store = {store}>
   <BrowserRouter>
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <PersistGate persistor={persistor}>
+        <App />
+  </PersistGate>
   </BrowserRouter>,
+  </Provider>,
   document.getElementById('root')
 
 );
